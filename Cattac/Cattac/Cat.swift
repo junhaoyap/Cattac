dce672b73dbd721c97b7dbd7f05eb8ea486fa7a3
MrFool <lordyyap@gmail.com>
2015-04-03 00:14:03 +0800

    Initial representation of cat, doodad, item and their constants

--- Cattac/Cattac/Cat.swift
+++ Cattac/Cattac/Cat.swift
@@ -5,5 +5,17 @@
 import Foundation
 
 class Cat {
-    // nothing for now
+    var name: String!
+    var hp: Int!
+    var puiDmg: Int!
+    var fartDmg: Int!
+    var poopDmg: Int!
+    
+    init(catName: String, catHp: Int, catPuiDmg: Int, catFartDmg: Int) {
+        name = catName
+        hp = catHp
+        puiDmg = catPuiDmg
+        fartDmg = catFartDmg
+        poopDmg = Constants.catAttributes.poopDmg
+    }
 }
--- Cattac/Cattac/CatFactory.swift
+++ Cattac/Cattac/CatFactory.swift
@@ -3,5 +3,47 @@
 */
 
 class CatFactory {
-    // nothing for now
+    
+    func createCat(nameOfCatToCreate: String) -> Cat? {
+        var catToReturn: Cat?
+        
+        switch nameOfCatToCreate {
+        case Constants.catName.nyanCat:
+            let catAttributes = Constants.catAttributes.nyanCatAttributes
+            catToReturn = Cat(
+                catName: nameOfCatToCreate,
+                catHp: catAttributes[0],
+                catPuiDmg: catAttributes[1],
+                catFartDmg: catAttributes[2]
+            )
+        case Constants.catName.nalaCat:
+            let catAttributes = Constants.catAttributes.nalaCatAttributes
+            catToReturn = Cat(
+                catName: nameOfCatToCreate,
+                catHp: catAttributes[0],
+                catPuiDmg: catAttributes[1],
+                catFartDmg: catAttributes[2]
+            )
+        case Constants.catName.grumpyCat:
+            let catAttributes = Constants.catAttributes.grumpyCatAttributes
+            catToReturn = Cat(
+                catName: nameOfCatToCreate,
+                catHp: catAttributes[0],
+                catPuiDmg: catAttributes[1],
+                catFartDmg: catAttributes[2]
+            )
+        case Constants.catName.pusheenCat:
+            let catAttributes = Constants.catAttributes.pusheenCatAttributes
+            catToReturn = Cat(
+                catName: nameOfCatToCreate,
+                catHp: catAttributes[0],
+                catPuiDmg: catAttributes[1],
+                catFartDmg: catAttributes[2]
+            )
+        default:
+            break
+        }
+
+        return catToReturn
+    }
 }
