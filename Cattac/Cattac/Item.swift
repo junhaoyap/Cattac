1156692f0ea9b94874de20e74ef3eacc67b92d57
Steven Khong <khongwh@gmail.com>
2015-04-05 05:21:51 +0800

    Make ItemFactory singleton

--- Cattac/Cattac/Item.swift
+++ Cattac/Cattac/Item.swift
@@ -4,10 +4,30 @@
 
 import Foundation
 
+enum ItemType {
+    case Milk, Rock, Nuke, Laser
+}
+
 class Item {
     var name: String!
     
     init(itemName: String) {
         name = itemName
     }
+}
+
+class MilkItem: Item {
+    
+}
+
+class RockItem: Item {
+    
+}
+
+class NukeItem: Item {
+    
+}
+
+class LaserItem: Item {
+    
 }
