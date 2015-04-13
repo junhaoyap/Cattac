9b18f65d9c8bc25f579ea8f93d984c6d13e720fe
Steven Khong <khongwh@gmail.com>
2015-04-13 15:04:59 +0800

    Fix attribute modification decay bug, refactor and document Cat class

--- Cattac/Cattac/Cat.swift
+++ Cattac/Cattac/Cat.swift
@@ -10,49 +10,55 @@ class Cat: TileEntity {
     private let baseDefence: Int
     private let baseMoveRange: Int
     private let baseFartRange: Int
+    private var basePuiDmg: Int
+    private var baseFartDmg: Int
+    private var basePoopDmg: Int
     
     var name: String!
     var hp: Int
-    var puiDmg: Int!
-    var fartDmg: Int!
-    var poopDmg: Int!
     
     var defenceMods = [StatModification]()
     var dmgMods = [StatModification]()
     var moveRangeMods = [StatModification]()
     var fartRangeMods = [StatModification]()
     
+    /// Cat's defence attribute with modifications
     var defence: Int {
-        var def = baseDefence
-        for mod in defenceMods {
-            def += mod.modification
-        }
-        return def
+        return applyAttrMods(baseDefence, mods: defenceMods)
     }
     
+    /// Cat's movement range attribute with modifications
     var moveRange: Int {
-        var range = baseMoveRange
-        for mod in moveRangeMods {
-            range += mod.modification
-        }
-        return range
+        return applyAttrMods(baseMoveRange, mods: moveRangeMods)
     }
     
+    /// Cat's fart range attribute with modifications
     var fartRange: Int {
-        var range = baseFartRange
-        for mod in fartRangeMods {
-            range += mod.modification
-        }
-        return range
+        return applyAttrMods(baseFartRange, mods: fartRangeMods)
+    }
+    
+    /// Cat's Pui damage attribute with modifications
+    var puiDmg: Int {
+        return applyAttrMods(basePuiDmg, mods: dmgMods)
+    }
+    
+    /// Cat's Fart damage attribute with modifications
+    var fartDmg: Int {
+        return applyAttrMods(baseFartDmg, mods: dmgMods)
+    }
+    
+    /// Cat's Poop damage attribute with modifications
+    var poopDmg: Int {
+        return applyAttrMods(basePoopDmg, mods: dmgMods)
     }
     
     init(catName: String, catHp: Int, catDef: Int, catPuiDmg: Int, catFartDmg: Int) {
         name = catName
         hp = catHp
+        basePuiDmg = catPuiDmg
+        baseFartDmg = catFartDmg
+        basePoopDmg = Constants.catAttributes.poopDmg
         baseDefence = catDef
-        puiDmg = catPuiDmg
-        fartDmg = catFartDmg
-        poopDmg = Constants.catAttributes.poopDmg
         baseMoveRange = Constants.catAttributes.moveRange
         baseFartRange = Constants.catAttributes.fartRange
         
@@ -70,10 +76,19 @@ class Cat: TileEntity {
         }
     }
     
+    /// Inflicts the damage on to cat's hp,
+    /// after applying cat's defence attribute.
+    /// cat.hp -= damage * 1/defence
+    ///
+    /// :param: damage Points to be reduced from hp
     func inflict(damage: Int) {
         self.hp -= damage * 1/defence
     }
     
+    /// Heals cat's HP directly.
+    /// cat.hp += hp
+    ///
+    /// :param: hp Points to increase the hp
     func heal(hp: Int) {
         self.hp += hp
     }
@@ -86,10 +101,36 @@ class Cat: TileEntity {
         return _sprite
     }
     
+    /// Called after execution of each game turn, to decay
+    /// attribute modifications.
     func postExecute() {
         moveRangeMods = moveRangeMods.filter {
             (var mod) in
             return --mod.life > 0
         }
+        defenceMods = defenceMods.filter {
+            (var mod) in
+            return --mod.life > 0
+        }
+        fartRangeMods = fartRangeMods.filter {
+            (var mod) in
+            return --mod.life > 0
+        }
+        dmgMods = dmgMods.filter {
+            (var mod) in
+            return --mod.life > 0
+        }
+    }
+    
+    /// Helper method to apply attribute modifications.
+    ///
+    /// :param: baseValue Base attribute of the cat
+    /// :param: mods StatModifications to be applied to base attribute value
+    private func applyAttrMods(baseValue: Int, mods: [StatModification]) -> Int {
+        var value = baseValue
+        for mod in dmgMods {
+            value += mod.modification
+        }
+        return value
     }
 }
