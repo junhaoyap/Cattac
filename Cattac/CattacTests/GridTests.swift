e38cef7aa5482a4579fda0f83fbc09ac068cf0a4
Wu Di <justwudi@gmail.com>
2015-04-13 19:21:26 +0800

    Add obstructed path test for grid tests

--- Cattac/CattacTests/GridTests.swift
+++ Cattac/CattacTests/GridTests.swift
@@ -2,7 +2,7 @@ import XCTest
 
 class GridTests: XCTestCase {
 
-    func createGrid(modify: (Grid)->()) -> Grid {
+    func createGrid(modify: (Grid)->Grid) -> Grid {
         var grid = Grid(rows: 10, columns: 10)
 
         for row in 0..<10 {
@@ -12,21 +12,49 @@ class GridTests: XCTestCase {
             }
         }
 
-        modify(grid)
-
         grid.constructGraph()
+
+        modify(grid)
 
         return grid
     }
 
-    func testShortestPathFromNode() {
-        var grid = createGrid({ (grid) in () })
+    func testShortestPathFromNodeWithoutObstruction() {
+        var grid = createGrid({ (grid) in return grid })
 
-        let fromNode = grid[0, 0]!
-        let toNode = grid[9, 9]!
+        let startNode = grid[0, 0]!
+        let endNode = grid[9, 9]!
 
-        let path = grid.shortestPathFromNode(fromNode, toNode: toNode)
+        let path = grid.shortestPathFromNode(startNode, toNode: endNode)
 
         XCTAssertEqual(path.count, 18, "Shortest path is incorrect")
+    }
+
+    func testShortestPathFromNodeWithObstruction() {
+        var grid = createGrid({ (grid) in
+            // Removes nodes from graph so that there is only nodes for the 
+            // left, top and right side of the grid.
+            for row in 0..<9 {
+                for column in 1..<9 {
+                    grid.removeNodeFromGraph(grid[row, column]!)
+                }
+            }
+            return grid
+        })
+
+        let originNode = grid[0, 0]!
+        let topRightNode = grid[9, 9]!
+        let bottomRightNode = grid[0, 9]!
+
+        // Get path from bottom left to top right
+        var path = grid.shortestPathFromNode(originNode, toNode: topRightNode)
+
+        XCTAssertEqual(path.count, 18, "Shortest path is incorrect")
+
+        // Get path from bottom left to bottom right
+        path = grid.shortestPathFromNode(originNode, toNode: bottomRightNode)
+
+        XCTAssertEqual(path.count, 27, "Shortest path is incorrect")
+
     }
 }
