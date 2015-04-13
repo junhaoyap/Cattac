6bf8852e421e2de704fa369fd579e98e9b6b0830
Steven Khong <khongwh@gmail.com>
2015-04-13 15:23:29 +0800

    Document other game objects

--- Cattac/Cattac/StatModification.swift
+++ Cattac/Cattac/StatModification.swift
@@ -1,6 +1,13 @@
 
+/// Modification for cat attribute. Allows for 
+/// multiple modifications on a single attribute 
+/// that is decayed over a pre-defined turn.
 class StatModification {
+    
+    /// Attribute modification amount.
     var modification: Int = 0
+    
+    /// Turns this modification will be in effect.
     var life: Int = 0
     
     init(_ modification: Int, life: Int) {
