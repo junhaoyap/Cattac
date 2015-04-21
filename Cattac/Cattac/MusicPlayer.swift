8a8fd16edad27e74fb9bb8b185f641a5351998f6
MrFool <lordyyap@gmail.com>
2015-04-22 01:19:02 +0800

    Added sound files and instantiated them

--- Cattac/Cattac/MusicPlayer.swift
+++ Cattac/Cattac/MusicPlayer.swift
@@ -30,4 +30,8 @@ class MusicPlayer {
     func playBackgroundMusic() {
 //        nyanBackgroundMusicPlayer.play()
     }
+    
+    func stopBackgroundMusic() {
+//        nyanBackgroundMusicPlayer.stop()
+    }
 }
