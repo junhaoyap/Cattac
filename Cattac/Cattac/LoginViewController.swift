f8be9f38ec946add8d0e2998c36a3f9c137568a7
MrFool <lordyyap@gmail.com>
2015-04-21 23:29:01 +0800

    Added back auto login

--- Cattac/Cattac/LoginViewController.swift
+++ Cattac/Cattac/LoginViewController.swift
@@ -21,7 +21,7 @@ class LoginViewController: UIViewController {
         backgroundMusicPlayer.playBackgroundMusic()
         
         // For testing and demo purposes only
-//        gameConnectionManager.autoLogin1(self)
+        gameConnectionManager.autoLogin1(self)
     }
     
     override func didReceiveMemoryWarning() {
